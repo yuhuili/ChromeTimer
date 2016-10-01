a0cc89ae96044b7e08479f31234ab27ef00a7692
Jay <imjching@users.noreply.github.com>
2016-10-01 16:58:41 -0400

    clean up some css and add jquery

--- countdown.js
+++ countdown.js
@@ -1,63 +1,55 @@
-window.onload = function start () {
-    
-    document.getElementById('description').style.visibility = "hidden";
-    var userinput = document.getElementById('form1');
-    userinput.elements[7].addEventListener("click", onSubmit);
-    
-    console.log(userinput.elements[7]);
-    
+$(function() {
+  document.getElementById('description').style.visibility = "hidden";
+  $("#datepicker").datepicker();
+  $("#submitForm").click(onSubmit);
+});
+
+function onSubmit() {
+  document.getElementById('form1').style.visibility = "hidden";
+  var description = document.getElementById('description');
+  description.style.visibility = "visible";
+
+  var userinput = document.getElementById('form1');
+
+  description.innerHTML = "Time Left Until " + userinput.elements[0].value;
+
+  var deadline = new Date(Date.parse($("#datepicker").datepicker("getDate")));
+  deadline.setHours($('[name="deadline_hour"]').val());
+  deadline.setMinutes($('[name="deadline_minute"]').val());
+  deadline.setSeconds($('[name="deadline_second"]').val());
+
+  getTimeRemaining(deadline);
+  displayClock(deadline);
 }
 
-function onSubmit (){
-        console.log("hi");
-        document.getElementById('form1').style.visibility = "hidden";
-        var description = document.getElementById('description');
-        console.log(document.getElementById('description'));
-        description.style.visibility = "visible";
-    
-        var userinput = document.getElementById('form1');
-    
-        description.innerHTML = "Time Left Until " + userinput.elements[0].value;
-    
-        console.log(userinput);
-        var deadline =  
-        userinput.elements[1].value + '-' + userinput.elements[2].value + '-' + userinput.elements[3].value + ' ' + userinput.elements[4].value + ':' + userinput.elements[5].value + ":" + userinput.elements[6].value;
+function getTimeRemaining(deadline) {
+  var now = new Date();
+  var t = deadline - Date.parse(now);
 
-    
-        getTimeRemaining(deadline);
-        displayClock(deadline);
+  var seconds = Math.floor((t/1000) % 60);
+  var minutes = Math.floor((t/1000/60) % 60);
+  var hours = Math.floor((t/1000/60/60)% 24);
+  var days = Math.floor((t/1000/60/60/24));
+
+  return {
+      'total':t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+  }
 }
 
-function getTimeRemaining (deadline){
-    var now = new Date();
-    var t = Date.parse(deadline) - Date.parse(now);
-    
-    var seconds = Math.floor((t/1000) % 60);
-    var minutes = Math.floor((t/1000/60) % 60);
-    var hours = Math.floor((t/1000/60/60)% 24);
-    var days = Math. floor((t/1000/60/60/24));
-    
-    return{
-        'total':t,
-        'days': days,
-        'hours': hours,
-        'minutes': minutes,
-        'seconds': seconds
+function displayClock(deadline) {
+  var timeinterval = setInterval(function(){
+    var time = getTimeRemaining(deadline);
+    var anothertimer = document.getElementById('anothertimer');
+
+    document.getElementById('time').innerHTML = time.days + " Days, " + time.hours + " Hours, " + time.minutes + " Minutes, " + time.seconds + " Seconds";
+
+    if (time.total <= 0) {
+      clearInterval(timeinterval);
+      document.getElementById('form1').style.visibility="visible";
     }
+  }, 1000)
 }
-
-function displayClock (deadline){    
-    var timeinterval = setInterval (function (){
-         var time = getTimeRemaining(deadline);
-         var anothertimer = document.getElementById('anothertimer');
-        
-         document.getElementById('time').innerHTML = time.days + " Days, " + time.hours + " Hours, " + time.minutes + " Minutes, " + time.seconds + " Seconds";
-        
-        if (time.total <= 0){
-            clearInterval(timeinterval);
-            document.getElementById('form1').style.visibility="visible";
-        }
-    }, 1000)
-}
-
-
